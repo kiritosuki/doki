2329863a11e619242520740dc7126fef42fa27d3
kirito <3322640054@qq.com>
2025-12-02 22:53:30 +0800

    clear all

--- internal/container/process.go
+++ internal/container/process.go
@@ -1,15 +1,1 @@
 package container
-
-import (
-	"os/exec"
-
-	"golang.org/x/sys/unix"
-)
-
-func NewParentProcess(tty bool, command string) *exec.Cmd {
-	args := []string{"init", command}
-	cmd := exec.Command("/proc/self/exe", args...)
-	cmd.SysProcAttr = &unix.SysProcAttr{
-		Cloneflags:
-	}
-}
